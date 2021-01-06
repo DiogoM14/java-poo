439ca6011a222fa65256110598bff68ed7115147
Xavi002 <diogomartins200214@gmail.com>
2021-01-06 10:44:18 +0000

    aula 1

---
+++ aula02/src/main/java/aula02/App.java
@@ -0,0 +1,15 @@
+package aula02;
+
+import main.java.aula02.Caneta;
+
+public class App 
+{
+    public static void main( String[] args ) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.bico = 0.5f;
+        c1.destapar();
+        c1.status(); 
+        c1.rabiscar();
+    }
+}
